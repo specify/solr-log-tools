cecffc09e3d8c1f385d12b049e435239bc7920c1
Ben Anhalt <anhalt@ku.edu>
2016-01-25 12:54:07 -0600

    update for 2016

--- queries.sql
+++ queries.sql
@@ -1,25 +1,25 @@
 select collection, count(*) as total_queries
 from solr_log
-where start_row = 0 and timestamp < '2015-01-01'
+where start_row = 0 and timestamp >= '2015-01-01' and timestamp < '2016-01-01'
 group by collection order by collection;
 
 select collection, count(distinct query) as distinct_queries
 from solr_log
-where start_row = 0 and timestamp < '2015-01-01'
+where start_row = 0 and timestamp >= '2015-01-01' and timestamp < '2016-01-01'
 group by collection order by collection;
 
 select collection, extract(month from timestamp) as month, count(*) as total_queries
 from solr_log
-where start_row = 0 and timestamp < '2015-01-01'
+where start_row = 0 and timestamp >= '2015-01-01' and timestamp < '2016-01-01'
 group by collection, month order by collection, month;
 
 select collection, query, count(*) as query_count, max(hits) as max_hits
 from solr_log
-where start_row = 0 and timestamp < '2015-01-01'
+where start_row = 0 and timestamp >= '2015-01-01' and timestamp < '2016-01-01'
 group by collection, query having count(*) > 5
 order by collection, query_count desc;
 
 select collection, sum(hits) as total_hits
 from solr_log
-where start_row = 0 and timestamp < '2015-01-01'
+where start_row = 0 and timestamp >= '2015-01-01' and timestamp < '2016-01-01'
 group by collection order by collection;
